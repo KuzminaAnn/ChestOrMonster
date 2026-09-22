376ed118437f0d2239274bc53fdf6d1ca0e432a3
234983 <nna905k@icloud.com>
2026-09-22 09:30:42 +0300

    Создали кот

---
+++ ChestOrMonster/Model/Enemy/Cat.cs
@@ -0,0 +1,34 @@
+﻿using ChestOrMonster.Interface;
+
+namespace ChestOrMonster.Model.Enemy
+{
+    internal class Cat : BaseEntity
+    {
+        public override string Name { get; }
+        public override double Hp { get; protected set; }
+        public override double Atk { get; }
+        public override double Def { get; }
+        public override DamageType AttackType { get; }
+        public override StatusEffect Effect { get; protected set; }
+
+        public Cat()
+        {
+            Name = "Кот";
+            Hp = 8;
+            Atk = 7;
+            Def = 1;
+            AttackType = DamageType.Usual;
+            Effect = StatusEffect.None;
+        }
+
+        public override DamageInfo Attack()
+        {
+            double finalAtk = Atk;
+            if (Hp <= 4)
+            {
+                finalAtk += 10;
+            }
+            return new DamageInfo(finalAtk, AttackType);
+        }
+    }
+}
---
+++ ChestOrMonster/Model/Item/Bow.cs
@@ -0,0 +1,17 @@
+﻿using ChestOrMonster.Interface;
+
+
+namespace ChestOrMonster.Model.Item
+{
+    internal class Bow : Weapon
+    {
+        public string Name { get; private set; }
+        public double Damage { get; private set; }
+
+        //public Bow ()
+        //{
+        //    Name = "Лук";
+        //    Damage = 17;
+        //}
+    }
+}
